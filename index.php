de231ab7153ad8dded4318e6f4db991435c9e8e6
dokodex <pfadarraweb@gmail.com>
2018-10-23 16:40:11 +0200

    begin

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'php/languages.php';?>
+<?php require_once('components/'. $language .'/header.php'); ?>
+<?php require_once('components/'. $language .'/home.php'); ?>
+<?php require_once('components/'. $language .'/footer.php'); ?>
