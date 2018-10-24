b4628288012db73db2e9eb67879cf280762fb3d9
dokodex <pfadarraweb@gmail.com>
2018-10-24 19:21:59 +0200

    design/puntos-venta

---
+++ components/eus/puntos-venta.php
@@ -0,0 +1,107 @@
+<section class="section puntos-venta">
+  <div class="puntos-venta__title title">SALTOKIAK</div>
+  <div class="puntos-venta__puntos-venta">
+    <div class="puntos-venta__punto-venta">
+      <div class="puntos-venta__punto-venta__grid">
+        <div class="puntos-venta__hover">
+          <a class="puntos-venta__hover__link" href="">
+            <div class="puntos-venta__maps"><?php require('imports/svg/maps.svg');?></div>
+            <img class="puntos-venta__img" src="img/puntos-venta/hernani.jpg" alt=""/>
+          </a>
+        </div>
+        <div class="puntos-venta__info">
+          <h2 class="puntos-venta__tienda">HERNANI</h2>
+          <div class="puntos-venta__location"><a class="puntos-venta__location__link"><?php require('imports/svg/location.svg');?>Polgono Akarregi, Pab.3A 20120, Hernani</a></div>
+          <div class="puntos-venta__tlf"><?php require('imports/svg/phone.svg');?>943 52 16 50</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/mail.svg');?>info@basmodec.com</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/clock.svg');?>L-S: 08:00 - 19:00</div>
+        </div>
+      </div>
+    </div>
+    <div class="puntos-venta__punto-venta">
+      <div class="puntos-venta__punto-venta__grid">
+        <div class="puntos-venta__hover">
+          <a class="puntos-venta__hover__link" href="">
+            <div class="puntos-venta__maps"><?php require('imports/svg/maps.svg');?></div>
+            <img class="puntos-venta__img" src="img/puntos-venta/hernani.jpg" alt=""/>
+          </a>
+        </div>
+        <div class="puntos-venta__info">
+          <h2 class="puntos-venta__tienda">HERNANI</h2>
+          <div class="puntos-venta__location"><a class="puntos-venta__location__link"><?php require('imports/svg/location.svg');?>Polgono Akarregi, Pab.3A 20120, Hernani</a></div>
+          <div class="puntos-venta__tlf"><?php require('imports/svg/phone.svg');?>943 52 16 50</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/mail.svg');?>info@basmodec.com</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/clock.svg');?>L-S: 08:00 - 19:00</div>
+        </div>
+      </div>
+    </div>
+    <div class="puntos-venta__punto-venta">
+      <div class="puntos-venta__punto-venta__grid">
+        <div class="puntos-venta__hover">
+          <a class="puntos-venta__hover__link" href="">
+            <div class="puntos-venta__maps"><?php require('imports/svg/maps.svg');?></div>
+            <img class="puntos-venta__img" src="img/puntos-venta/hernani.jpg" alt=""/>
+          </a>
+        </div>
+        <div class="puntos-venta__info">
+          <h2 class="puntos-venta__tienda">HERNANI</h2>
+          <div class="puntos-venta__location"><a class="puntos-venta__location__link"><?php require('imports/svg/location.svg');?>Polgono Akarregi, Pab.3A 20120, Hernani</a></div>
+          <div class="puntos-venta__tlf"><?php require('imports/svg/phone.svg');?>943 52 16 50</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/mail.svg');?>info@basmodec.com</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/clock.svg');?>L-S: 08:00 - 19:00</div>
+        </div>
+      </div>
+    </div>
+    <div class="puntos-venta__punto-venta">
+      <div class="puntos-venta__punto-venta__grid">
+        <div class="puntos-venta__hover">
+          <a class="puntos-venta__hover__link" href="">
+            <div class="puntos-venta__maps"><?php require('imports/svg/maps.svg');?></div>
+            <img class="puntos-venta__img" src="img/puntos-venta/hernani.jpg" alt=""/>
+          </a>
+        </div>
+        <div class="puntos-venta__info">
+          <h2 class="puntos-venta__tienda">HERNANI</h2>
+          <div class="puntos-venta__location"><a class="puntos-venta__location__link"><?php require('imports/svg/location.svg');?>Polgono Akarregi, Pab.3A 20120, Hernani</a></div>
+          <div class="puntos-venta__tlf"><?php require('imports/svg/phone.svg');?>943 52 16 50</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/mail.svg');?>info@basmodec.com</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/clock.svg');?>L-S: 08:00 - 19:00</div>
+        </div>
+      </div>
+    </div>
+    <div class="puntos-venta__punto-venta">
+      <div class="puntos-venta__punto-venta__grid">
+        <div class="puntos-venta__hover">
+          <a class="puntos-venta__hover__link" href="">
+            <div class="puntos-venta__maps"><?php require('imports/svg/maps.svg');?></div>
+            <img class="puntos-venta__img" src="img/puntos-venta/hernani.jpg" alt=""/>
+          </a>
+        </div>
+        <div class="puntos-venta__info">
+          <h2 class="puntos-venta__tienda">HERNANI</h2>
+          <div class="puntos-venta__location"><a class="puntos-venta__location__link"><?php require('imports/svg/location.svg');?>Polgono Akarregi, Pab.3A 20120, Hernani</a></div>
+          <div class="puntos-venta__tlf"><?php require('imports/svg/phone.svg');?>943 52 16 50</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/mail.svg');?>info@basmodec.com</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/clock.svg');?>L-S: 08:00 - 19:00</div>
+        </div>
+      </div>
+    </div>
+    <div class="puntos-venta__punto-venta">
+      <div class="puntos-venta__punto-venta__grid">
+        <div class="puntos-venta__hover">
+          <a class="puntos-venta__hover__link" href="">
+            <div class="puntos-venta__maps"><?php require('imports/svg/maps.svg');?></div>
+            <img class="puntos-venta__img" src="img/puntos-venta/hernani.jpg" alt=""/>
+          </a>
+        </div>
+        <div class="puntos-venta__info">
+          <h2 class="puntos-venta__tienda">HERNANI</h2>
+          <div class="puntos-venta__location"><a class="puntos-venta__location__link"><?php require('imports/svg/location.svg');?>Polgono Akarregi, Pab.3A 20120, Hernani</a></div>
+          <div class="puntos-venta__tlf"><?php require('imports/svg/phone.svg');?>943 52 16 50</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/mail.svg');?>info@basmodec.com</div>
+          <div class="puntos-venta__email"><?php require('imports/svg/clock.svg');?>L-S: 08:00 - 19:00</div>
+        </div>
+      </div>
+    </div>
+  </div>  
+</section>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include 'php/languages.php';?>
 <?php require_once('components/'. $language .'/header.php'); ?>
 <?php require_once('components/'. $language .'/home.php'); ?>
+<?php require_once('components/'. $language .'/puntos-venta.php'); ?>
 <?php require_once('components/'. $language .'/footer.php'); ?>
