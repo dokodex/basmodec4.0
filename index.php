08fa909d990475fe331415b12784881eb759af76
dokodex <pfadarraweb@gmail.com>
2018-10-25 17:32:35 +0200

    design/noticias

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 <?php require_once('components/'. $language .'/header.php'); ?>
 <?php require_once('components/'. $language .'/home.php'); ?>
 <?php require_once('components/'. $language .'/puntos-venta.php'); ?>
+<?php require_once('components/'. $language .'/noticias.php'); ?>
 <?php require_once('components/'. $language .'/footer.php'); ?>
