4bef9cf0b8ad0dddd9bf47da5213791de0c2b688
dokodex <pfadarraweb@gmail.com>
2018-12-04 17:33:50 +0100

    fix/footer

--- components/eus/brikolatu.php
+++ components/eus/brikolatu.php
@@ -1,6 +1,6 @@
 <section class="section brikolatu">
   <div class="brikolatu__grid">
     <div class="brikolatu__divimg vcenter"><a class="brikolatu__img__link" href="http://brikolatu.com/" target="_blank"><img class="brikolatu__img" src="img/brikolatu/brikolatu.png" alt=""/></a></div>
-    <div class="brikolatu__maps"><iframe src="https://www.google.com/maps/d/embed?mid=1muW7wprSFAJtxHteWT1RbkQRTcAnD9iK&hl=es" width="440" height="300"></iframe></div>
+    <div class="brikolatu__maps"><iframe class="brikolatu__maps__iframe" src="https://www.google.com/maps/d/embed?mid=1muW7wprSFAJtxHteWT1RbkQRTcAnD9iK&hl=es" width="440" height="300"></iframe></div>
   </div>
 </section>
